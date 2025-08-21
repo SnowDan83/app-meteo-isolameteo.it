d15b8502917cd6655a640b6f72ba449fe5894a52
SnowDan83 <danielesanna1983@gmail.com>
2025-08-21 21:27:23 +0200

    Desktop Upload

--- app_meteo.bat
+++
@@ -1,1 +0,0 @@
-python START.py
--- app_meteo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/danyslipknot/Insync/danielesanna1983@gmail.com/Google\ Drive/Informatica/isolameteo.it/app_meteo
-#xhost +si:localuser:danyslipknot
-python START.pyw
