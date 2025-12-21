84d71b300eca6166208e0e779a614d2ed1dabb45
Daniele <danielesanna1983@gmail.com>
2025-12-21 09:02:09 +0100

    v10.5

--- PNG.py
+++ PNG.py
@@ -1,77 +1,79 @@
 #!/usr/bin/env python3
 #-----Librerie-----#
 import urllib.request as urllib2
-from wand.image import Image
-from wand.drawing import Drawing
-from wand.color import Color
+from wand.image import Image    #Creazione immagine
+from wand.display import display    #Creazione immagine
+from wand.drawing import Drawing    #Creazione immagine
+from wand.color import Color    #Creazione immagine
+from typing import NewType
 import os
 from os import path
 import shutil
 import INTERFACE    
 #-----Fine Librerie e Import-----#
 
+
+#-----Inizializzazione variabili globali-----#
+#Array 'coordx[]' per coordinate posizionamento immagini condizioni
+coordx=[55,155,75,160,65,160,345,445,365,455,345,455,645,745,665,755,665,755]
+#Array 'coordy[]' per coordinate posizionamento immagini condizioni
+coordy=[75,30,195,160,320,280,75,30,195,160,320,280,75,30,195,160,320,280]
+#Array 'coordv[]' per coordinate posizionamento immagini venti
+coordv=[90,30,255,170,385,325,550,465,690,620,845,765]
+#Array 'coordw[]' per coordinate posizionamento immagini venti
+coordw=[30,240,215,390,30,240,215,390,30,240,215,390]
+#-----Fine inizializzazione variabili globali-----#
+
+
+#--------------------Classe PNG_Maker--------------------#
 class PNG_Maker():
-    def __init__(self, giuorno, nuome): 
-        o = INTERFACE.UI()     
-        self.makeImage(o, giuorno, nuome)   
 
-    def makeImage(self, o, giuorno, nuome): 
-        # [Logica icone e temperature invariata]
-        coordx=[55,155,75,160,65,160,345,445,365,455,345,455,645,745,665,755,665,755]
-        coordy=[75,30,195,160,320,280,75,30,195,160,320,280,75,30,195,160,320,280]
-        coordv=[90,30,255,170,385,325,550,465,690,620,845,765]
-        coordw=[30,240,215,390,30,240,215,390,30,240,215,390]
+    def __init__(self, giuorno, nuome): #Costruttore della classe PNG_Maker
+        o = INTERFACE.UI()     #Creazione oggetto di classe UI
+        self.makeImage(o, giuorno, nuome)   #Metodo tramite il quale si creano le immagini, con il passaggio di giorno da stampare su immagine e nome con il quale salvare il file
 
-        for i in range(30): 
-            z = './base.png' if i == 0 else './base_prov.png'
-            tx = coordx[i] if i < 18 else coordv[i-18]
-            ty = coordy[i] if i < 18 else coordw[i-18]
-            with Image(filename=z) as bg_img:   
-                with Image(filename=o.urlF[i]) as zona_img: 
-                    bg_img.composite(zona_img, left=tx, top=ty)   
-                    bg_img.save(filename='./base_prov.png')   
 
-        with Image(filename='./base_prov.png') as bg_img: 
-            with Image(filename=o.urlF[30]) as temp_img:    
-                bg_img.composite(temp_img, left=545, top=370)   
-                bg_img.save(filename='quasi.png')   
+    def makeImage(self, o, giuorno, nuome): #Metodo principale classe PNG_Maker
+        for i in range(30): #30 iterazioni, una per ogni immagine delle condizioni da applicare
+            if i<18:    #Posizione delle condizioni
+                if i==0:    #Scelta prima immagine da usare come base
+                    z='./base.png'
+                else:
+                    z='./base_prov.png'
+                with Image(filename=z) as bg_img:   #Scelta immagine
+                    with Image(filename=o.urlF[i]) as zona_img: #Scelta immagine
+                        bg_img.composite(zona_img, left=coordx[i], top=coordy[i])   #Composizione immagine
+                        bg_img.save(filename='./base_prov.png')   #Salvataggio immagine
+            elif i>=18: #Posizione dei venti
+                with Image(filename='./base_prov.png') as bg_img: #Scelta immagine
+                    with Image(filename=o.urlF[i]) as zona_img: #Scelta immagine
+                        bg_img.composite(zona_img, left=coordv[i-18], top=coordw[i-18]) #Composizione immagine
+                        bg_img.save(filename='./base_prov.png')   #Salvataggio immagine
 
-        #----- LOGICA DATA CROSS-DISTRO (Arch e Ubuntu) -----#
-        stringaGiorno = str(giuorno)
-        stringaNome = str(nuome)
+        #-----Applicazione Immagine Temperatura-----#
+        with Image(filename='./base_prov.png') as bg_img: #Scelta immagine
+            with Image(filename=o.urlF[30]) as temp_img:    #Scelta immagine
+                bg_img.composite(temp_img, left=545, top=370)   #Composizione immagine
+                bg_img.save(filename='quasi.png')   #Salvataggio immagine
+
+        #-----Applicazione Scritta Data-----
+        stringaGiorno = str(giuorno)    #Metodo parse per convertire il giorno
+        stringaNome = str(nuome)    #Metodo parse per convertire il nome del file
 
         with Drawing() as draw:
-            # Lista di percorsi reali per font "Sans" comuni su Arch e Ubuntu
-            font_paths = [
-                "/usr/share/fonts/TTF/DejaVuSans-Bold.ttf",          # Arch Linux (Standard)
-                "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", # Ubuntu (Standard)
-                "/usr/share/fonts/TTF/LiberationSans-Bold.ttf",      # Arch Alternative
-                "/usr/share/fonts/truetype/liberation/LiberationSans-Bold.ttf", # Ubuntu Alternative
-                "/usr/share/fonts/TTF/Arial.ttf"                     # Se hai i font MS su Arch
-            ]
-            
-            # Seleziona il primo font che esiste davvero sul disco
-            for p in font_paths:
-                if os.path.exists(p):
-                    draw.font = p
-                    break
-            
-            draw.font_size = 18
-            draw.fill_color = Color('yellow')
-            
-            # Aggiungiamo un'ombra nera (stroke) per garantire la leggibilità su Arch
-            draw.stroke_color = Color('black')
-            draw.stroke_width = 1
-            
-            with Image(filename="./quasi.png") as img:
-                # Disegna il testo
-                draw.text(480, 15, stringaGiorno) 
-                draw(img)
-                # Salva l'immagine finale
-                img.save(filename=stringaNome)
+          with Image(filename = "./quasi.png") as img:    #Scelta immagine
+            draw.font = 'Cantarell' #Font scritta
+            draw.font_size = 16 #Dimensione scritta
+            draw.fill_color=Color('yellow') #Colore scritta
+            draw.text(510, 22, stringaGiorno)   #Scrittura data
+            draw(img)
+            img.save(filename = stringaNome)    #Salvataggio immagine
+        img.close() #Chiusura immagine
 
-        # Spostamento file nella cartella 'pronte'
+        #Spostamento file
+        src = "./"
         dst = "./pronte"
-        if not os.path.exists(dst): os.makedirs(dst)
-        if os.path.exists(stringaNome):
-            shutil.move(stringaNome, os.path.join(dst, stringaNome))
+        files = [i for i in os.listdir(src) if i.startswith(stringaNome) and path.isfile(path.join(src, i))]
+        for f in files:
+            shutil.move(os.path.join(src, f), os.path.join(dst, f))
+#--------------------Fine Classe PNG_Maker--------------------#
